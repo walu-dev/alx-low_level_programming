73aa4ca8b1d53467490b76967487cef4cd7ebe13
walu-dev <jonathanenoch11@gmail.com>
2022-12-09 13:19:27 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * main - Entry piont
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+
+
+
+}
