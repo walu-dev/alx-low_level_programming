c36363bd282095c3c45c461b6aecb619612989f9
walu-dev <jonathanenoch11@gmail.com>
2022-12-13 07:39:03 -0800

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,13 @@
  */
 
 void print_alphabet_x10(void)
-
 {
 	int x;
 	char y;
 
 	for (x = 0; x  < 10; x++)
 	{
-		for (y = 'a'; y < 'z'; y++)
+		for (y = 'a'; y <= 'z'; y++)
 		{
 			_putchar(y);
 		}
