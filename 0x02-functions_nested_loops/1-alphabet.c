9d7c3f929388a09105ce3c5414ce5f7c9d505a84
walu-dev <jonathanenoch11@gmail.com>
2022-12-13 07:09:40 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
