b920cf3b60a56cec3d62bbbe5241926587b1d839
walu-dev <jonathanenoch11@gmail.com>
2023-01-10 16:27:10 -0800

    fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -29,6 +29,25 @@ int _strlen(char *s)
 
 char *_strcpy(char *dest, char *src)
 {
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strdup - array for prints a string
+ * @str: array of elements
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
 	char *dst;
 	unsigned int size;
 
